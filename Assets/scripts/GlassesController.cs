8520525b60d162038b5d28c15308f27b783bd256
Joel Hnatow <joel@hnatow.com>
2015-01-24 17:35:04 -0500

    wakeup scene shit

---
+++ Assets/scripts/GlassesController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlassesController : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log ("Glasses OnTriggerEnter2D Called");
+		
+		//audio.Play();
+		
+		//Destroy(gameObject, 2.0f);
+	}
+
+//	void OnTriggerEnter2D () {
+//
+//		Debug.Log ("Glasses OnTriggerEnter2D Called");
+//
+//		audio.Play();
+//
+//		Destroy(gameObject, 2.0f);
+//			
+//		//Destroy (GameObject.FindGameObjectWithTag("glasses"));
+//
+//	}
+
+}
