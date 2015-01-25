b91339180d2f637cfd3953b6f939b1df123d8ffd
Parker Evans <pae26@cornell.edu>
2015-01-25 00:41:15 -0500

    Added lots of stuff for bingo - mostly done, just needs some audio and transitions

--- Assets/scripts/bingo/game.cs
+++ Assets/scripts/bingo/game.cs
@@ -6,13 +6,17 @@ using System;
 public class game : MonoBehaviour {
 
 	int currentNumber;
+	List<int> calledNumbers;
 	int updateTimes;
+	bool gameOver;
 
 	// Granny's boards
 	private int[][,] boards;
+	private bool[][,] boardsStates;
 
 	// Opponent boards
 	private int[][,] opponentBoards;
+	private bool[][,] opponentBoardsStates;
 
 	// Random numbers used for the game and initialization
 	private System.Random rand;
@@ -23,23 +27,53 @@ public class game : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		currentNumber = -1;
+		calledNumbers = new List<int>();
 		updateTimes = 0;
+		gameOver = false;
 
 		// Generate the boards
 		rand = new System.Random ();
 
 		boards = new int[4][,];
+		boardsStates = new bool[4][,];
 		for (int i = 0; i < 4; i++) {
 			boards[i] = new int[5, 5];
 			GenerateBoard(ref boards[i]);
 
-			// TODO: Write out the visible board
+			boardsStates[i] = new bool[5, 5]
+			{
+				{false,false,false,false,false},
+				{false,false,false,false,false},
+				{false,false,false,false,false},
+				{false,false,false,false,false},
+				{false,false,false,false,false},
+			};
+
+			// Write out the visible board
+			for (int j = 0; j < 5; j++) {
+				for (int k = 0; k < 5; k++) {
+					// Don't put numbers on the free space
+					if (j != 2 || k != 2) {
+						PlaceNumberOnBoard(i, boards[i][j,k], j, k);
+					}
+				}
+			}
 		}
 
 		opponentBoards = new int[20][,];
+		opponentBoardsStates = new bool[20][,];
 		for (int i = 0; i < 20; i++) {
 			opponentBoards[i] = new int[5, 5];
 			GenerateBoard(ref opponentBoards[i]);
+
+			opponentBoardsStates[i] = new bool[5, 5]
+			{
+				{false,false,false,false,false},
+				{false,false,false,false,false},
+				{false,false,true,false,false},
+				{false,false,false,false,false},
+				{false,false,false,false,false},
+			};
 		}
 
 		// Initialize the number bucket
@@ -66,19 +100,47 @@ public class game : MonoBehaviour {
 		}
 	}
 
+	void PlaceNumberOnBoard(int board, int number, int i, int j) {
+		// Show a circle around the number on the board
+		GameObject num = Resources.Load (number.ToString()) as GameObject;
+		
+		Vector3 numPosition = new Vector3(0,0,0);
+		switch (board) {
+		case 0:
+			numPosition = new Vector3(-6.31f + i*0.53f, -1.19f + j*-0.53f, 0);
+			break;
+		case 1:
+			numPosition = new Vector3(-2.89f + i*0.53f, -1.19f + j*-0.53f, 0);
+			break;
+		case 2:
+			numPosition = new Vector3(0.77f + i*0.53f, -1.19f + j*-0.53f, 0);
+			break;
+		case 3:
+			numPosition = new Vector3(4.27f + i*0.53f, -1.19f + j*-0.53f, 0);
+			break;
+		}
+		
+		num = Instantiate (num, numPosition, Quaternion.identity) as GameObject;
+		num.transform.localScale = new Vector3 (1.5f, 1.5f, 0);
+		num.renderer.sortingOrder = 3;
+	}
+
 	void FixedUpdate() {
-		// TODO: Handle end of game
+		// Handle end of game w/ no winner
 		if (bucket.Count == 0) {
-			return;
+			// TODO: Handle weird no winner gameover case
+			Debug.Log("Game over: Nobody won!");
+			gameOver = true;
 		}
 
-		updateTimes++;
+		if (!gameOver) {
+			updateTimes++;
 
-		// This should be called 50 times a second (default physics update), update every 4s
-		if (updateTimes >= 50 * 4) {
-			updateTimes = 0;
-
-			PickANumber();
+			// This should be called 50 times a second (default physics update), update every 10s
+			if (updateTimes >= 50 * 10) {
+				updateTimes = 0;
+				PickANumber ();
+			}
 		}
 	}
 
@@ -88,11 +150,12 @@ public class game : MonoBehaviour {
 		// Pick a number
 		int index = rand.Next (0, bucket.Count);
 		currentNumber = bucket [index];
+		calledNumbers.Add(currentNumber);
 		bucket.RemoveAt (index);
 
 		// TODO: Announce the number
 
-		// TODO: Show a circle around the number on the board
+		// Show a circle around the number on the board
 		GameObject circle = Resources.Load ("circle") as GameObject;
 
 		// Hacky way to get position for circle
@@ -110,13 +173,122 @@ public class game : MonoBehaviour {
 		}
 		circle = Instantiate (circle, circlePosition, Quaternion.identity) as GameObject;
 		circle.transform.localScale = new Vector3 (2, 2, 0);
+		circle.renderer.sortingOrder = 3;
 
-		// TODO: Update the opponent states
-		// TODO: Handle any other winners
+		// Update the opponent states and handle any winners
+		for (int i = 0; i < 20; i++) {
+			for (int j = 0; j < 5; j++) {
+				for (int k = 0; k < 5; k++) {
+					if (opponentBoards[i][j,k] == currentNumber) {
+						opponentBoardsStates[i][j,k] = true;
+
+						// Check for a winner
+						if (CheckForWinner(ref opponentBoardsStates[i], j, k)) {
+							// TODO: Handle other winner
+							Debug.Log ("Game over: someone else won!");
+							gameOver = true;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	// Check for winner (optimized to only check rows and columns that were just updated and diagonals)
+	bool CheckForWinner(ref bool[,] board, int i, int j) {
+		if (board [0, 0] && board [1, 1] && board [2, 2] && board [3, 3] && board [4, 4]) {
+			return true;
+		} else if (board [0, 4] && board [1, 3] && board [2, 2] && board [3, 1] && board [4, 0]) {
+			return true;
+		} else if (board [i, 0] && board [i, 1] && board [i, 2] && board [i, 3] && board [i, 4]) {
+			return true;
+		} else if (board [0, j] && board [1, j] && board [2, j] && board [3, j] && board [4, j]) {
+			return true;
+		}
+
+		return false;
 	}
 
 	public void BoardClicked(string name) {
-		// TODO: Handle the board being clicked (if it has a number, update the state, otherwise bad granny game over)
+		// Handle the board being clicked (if it has a number, update the state, otherwise bad granny game over)
 		Debug.Log ("Board clicked: " + name);
+
+		int boardNum = 0;
+		if (name == "board1") {
+			boardNum = 0;
+		} else if (name == "board2") {
+			boardNum = 1;
+		} else if (name == "board3") {
+			boardNum = 2;
+		} else if (name == "board4") {
+			boardNum = 3;
+		}
+
+		// Update the granny board states and handle any winners
+		bool found = false;
+		for (int j = 0; j < 5; j++) {
+			for (int k = 0; k < 5; k++) {
+				if (calledNumbers.Contains(boards[boardNum][j,k]) && !boardsStates[boardNum][j,k]) {
+					found = true;
+					boardsStates[boardNum][j,k] = true;
+
+					// Place the piece
+					PlaceToken(boardNum, j, k);
+					
+					// Check for a winner
+					if (CheckForWinner(ref boardsStates[boardNum], j, k)) {
+						// TODO: Handle granny winner (add money)
+						GrannyState.moneyCt += 50;
+
+						Debug.Log("Game over: Granny won!!");
+						gameOver = true;
+					}
+
+					// Only allow granny to find one number at a time
+					goto haha_goto;
+				}
+			}
+		}
+
+	haha_goto:
+
+		// Handle granny cheating
+		if (!found) {
+			// Give her a chance to place a token on the free space
+			if (!boardsStates[boardNum][2,2]) {
+				boardsStates[boardNum][2,2] = true;
+
+				PlaceToken(boardNum, 2, 2);
+			} else {
+				// TODO: Granny cheated! Handle loss
+				Debug.Log("Game over: Granny cheated!");
+				gameOver = true;
+			}
+		}
+	}
+
+	void PlaceToken(int board, int i, int j) {
+		// Show a circle around the number on the board
+		GameObject x = Resources.Load ("bingo_x") as GameObject;
+
+		Vector3 xPosition = new Vector3(0,0,0);
+		switch (board) {
+		case 0:
+			xPosition = new Vector3(-6.31f + i*0.53f, -1.19f + j*-0.53f, 0);
+			break;
+		case 1:
+			xPosition = new Vector3(-2.89f + i*0.53f, -1.19f + j*-0.53f, 0);
+			break;
+		case 2:
+			xPosition = new Vector3(0.77f + i*0.53f, -1.19f + j*-0.53f, 0);
+			break;
+		case 3:
+			xPosition = new Vector3(4.27f + i*0.53f, -1.19f + j*-0.53f, 0);
+			break;
+		}
+
+		x = Instantiate (x, xPosition, Quaternion.identity) as GameObject;
+		x.transform.localScale = new Vector3 (2, 2, 0);
+		x.renderer.sortingOrder = 3;
 	}
 }
