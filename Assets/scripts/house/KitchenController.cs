42a84a2b7ec1010c75803c11313bd68888841606
Joel Hnatow <joel@hnatow.com>
2015-01-25 02:06:57 -0500

    More stuff, I forget.

---
+++ Assets/scripts/house/KitchenController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KitchenController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		GameObject toaster = GameObject.Find("Toaster");
+		SpriteRenderer toasterSprite = toaster.GetComponent<SpriteRenderer>();
+		ToasterController toasterControl = toaster.GetComponent<ToasterController>();
+
+		if (GrannyState.instance.hasGlasses == true) {
+			Debug.Log ("Granny got her glasses, hide em!!!");
+			Destroy(GameObject.Find ("kitchen-blur"));
+
+			toasterSprite.color = new Color(1f, 1f, 1f, 1f);
+			toasterControl.speed = 3f;
+
+		} else {
+			
+			toasterSprite.color = new Color(1f, 1f, 1f, 0.3f);
+			toasterControl.speed = 6f;
+
+		}
+	}
+}
